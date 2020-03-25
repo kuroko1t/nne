76b6f0a1141ca0c6a012aca3d4ef490f46f458ed
kuroko1t <kurosawa.yk@gmail.com>
2020-03-25 14:57:39 +0900

    update tf2.2 and edgetpu

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='torch2tflite',
     packages=find_packages(),
-    install_requires=['torch', 'onnx', 'tensorflow==1.15.2', 'onnx_tf', 'torchvision'],
+    install_requires=['torch', 'onnx', 'tensorflow', 'onnx_tf', 'torchvision'],
     version='0.1'
 )
--- torch2tflite/__init__.py
+++ torch2tflite/__init__.py
@@ -5,7 +5,9 @@ from onnx_tf.backend import prepare
 import tensorflow as tf
 import os
 
-def convert2tflite(model, dummy_input, tflite_path):
+#tf.enable_eager_execution()
+
+def convert2tflite(model, dummy_input, tflite_path, edgetpu=False):
     """
     convert torch model to tflite model using onnx
     """
@@ -20,23 +22,32 @@ def convert2tflite(model, dummy_input, tflite_path):
 
     os.system(f'onnx-tf convert -i {tmp_onnx_path} -o {tmp_pb_path}')
 
-    converter = tf.lite.TFLiteConverter.from_frozen_graph(
+    train = tf.convert_to_tensor(dummy_input.numpy())
+    my_ds = tf.data.Dataset.from_tensor_slices((train)).batch(10)
+
+    converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
         tmp_pb_path,
         onnx_input_names,
         onnx_output_names
     )
-    
+    if edgetpu:
+        def representative_dataset_gen():
+        for input_value in my_ds.take(10):
+            yield [input_value]
+        converter.representative_dataset = representative_dataset_gen
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = tf.int8
+        converter.inference_output_type = tf.int8
     tflite_model = converter.convert()
-    
+
     with open(tflite_path, 'wb') as f:
         f.write(tflite_model)
-
 
 def infer_tflite(tflitepath, input_data):
     interpreter = tf.lite.Interpreter(model_path="alexnet.tflite")
     # allocate memory
     interpreter.allocate_tensors()
-    
+
     # get model input and output propaty
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -45,7 +56,7 @@ def infer_tflite(tflitepath, input_data):
 
     # set tensor pointer to index
     interpreter.set_tensor(input_details[0]['index'], input_data)
-    
+
     # execute infer
     interpreter.invoke()
     # get result from index of output_details
