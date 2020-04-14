b25a3da5344f7991f5a15e3c384340de5c9987e8
kuroko <kurosawa.yk@gmail.com>
2020-04-14 17:58:35 +0900

    update example

--- examples/torch_example.py
+++ examples/torch_example.py
@@ -7,4 +7,4 @@ input_shape = (1, 3, 224, 224)
 model = torchvision.models.resnet34(pretrained=True).cuda()
 
 input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-output_data = nne.infer_torch(model, input_data, benchmark=True)
+output_data = nne.infer_torch(model, input_data)
