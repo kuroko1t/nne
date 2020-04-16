6c3eb709aac14565c74171d737cc7ff07a36b828
kuroko <kurosawa.yk@gmail.com>
2020-04-16 17:25:38 +0900

    update

--- nne/common.py
+++ nne/common.py
@@ -9,12 +9,11 @@ def check_jetson():
 
 def check_tensorrt():
     os.environ['LD_LIBRARY_PATH'] = "{os.environ['LD_LIBRARY_PATH']}:/usr/local/lib/64"
-    #try:
-    import tensorrt
-    return True
-    #except:
-    #    print('koko000')
-    #    return False
+    try:
+        import tensorrt
+        return True
+    except:
+        return False
 
 def check_model_is_cuda(model):
     return next(model.parameters()).is_cuda
