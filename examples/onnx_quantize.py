0af38e8b8899234e55f3a94acf3bda3106f86e3a
kuroko1t <kurosawa.yk@gmail.com>
2021-06-29 06:43:08 +0900

    update

--- examples/onnx_quantize.py
+++ examples/onnx_quantize.py
@@ -1,4 +1,13 @@
 from nne.quant.onnx import quant_oplist, quant_summary, quantize
+import torchvision
+import nne
+
+input_shape = (1, 3, 64, 64)
+onnx_file = 'resnet.onnx'
+model = torchvision.models.resnet34(pretrained=True)
+
+# convert pytorch model to onnx model
+nne.cv2onnx(model, input_shape, onnx_file)
 
 # onnx model to quantized model
 quantize("resnet.onnx")
